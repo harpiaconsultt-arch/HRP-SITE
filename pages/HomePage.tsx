4f299091b35d231b0115b85bf695ea2f906e7cc0
harpiaconsultt-arch <harpiaconsultt@gmail.com>
2026-03-02 11:50:13 -0300

    fix: Correct street name in contact details
    Updates all instances of "Avenida Olegário Maciel" to "Rua Olegário Maciel" in contact information across multiple blog posts. This rectifies an address detail to ensure accuracy.

---
+++ pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Harpia Consultoria</h1>
+      <p>Bem-vindo ao nosso site.</p>
+    </div>
+  );
+};
+
+export default HomePage;
