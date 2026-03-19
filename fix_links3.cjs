5c3fa309cde04eb4ba7121e260e9c82169dcb91e
harpiaconsultt-arch <harpiaconsultt@gmail.com>
2026-03-19 10:42:34 -0300

    feat: Enhance SEO and site structure
    Improves SEO meta tags for blog posts and adds new redirections for privacy policy in .htaccess. Updates sitemap to include new pages. Removes unnecessary Vite plugin.

--- fix_links3.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('index.html', 'utf-8');
-content = content.replace(/href=["']vistoria-cautelar-vizinhanca-juiz-de-fora\.html["']/g, 'href="blog/vistoria-cautelar-de-vizinhanca-juiz-de-fora.html"');
-content = content.replace(/https:\/\/www\.harpiaconsultoria\.eng\.br\/vistoria-cautelar-vizinhanca-juiz-de-fora\.html/g, 'https://harpiaconsultoria.eng.br/blog/vistoria-cautelar-de-vizinhanca-juiz-de-fora.html');
-fs.writeFileSync('index.html', content, 'utf-8');
-console.log('Done');
